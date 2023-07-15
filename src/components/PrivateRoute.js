75b2cccc847abb3897a760425f93261b94033e3e
Dorota Dudzik <113298700+doradudzik@users.noreply.github.com>
2023-07-15 16:38:57 +0200

    Proptypes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,15 @@
 import useAuth from 'hooks/useAuth';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
+};
+
+PrivateRoute.propTypes = {
+  redirectTo: PropTypes.string,
+  component: PropTypes.node.isRequired,
 };
